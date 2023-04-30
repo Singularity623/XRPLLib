964bea21a11205c8c56af25931188e49483b208c
Singularity <sheider623@gmail.com>
2023-04-30 18:17:37 -0500

    account and ledger header files
    account and ledger header files

---
+++ include/xrpl/account.h
@@ -0,0 +1,20 @@
+#ifndef XRPL_ACCOUNT_H
+#define XRPL_ACCOUNT_H
+
+#include <string>
+#include "utils.h"
+
+namespace xrpl 
+{
+    class Account 
+    {  
+        public:
+            Account(const std::string& address);
+            std::string getAccountInfo() const;
+        
+        private:
+            std::string address_;
+    };
+}
+
+#endif
---
+++ include/xrpl/ledger.h
@@ -0,0 +1,16 @@
+#ifndef XRPL_LEDGER_H
+#define XRPL_LEDGER_H
+
+#include <string>
+#include "utils.h"
+
+namespace xrpl 
+{
+    class Ledger 
+    {
+        public:
+            std::string getLatestValidatedLEdger() const;
+    };
+}
+
+#endif
